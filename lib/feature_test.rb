bbf59910013e505d2b19627e7ee1ed45def5bcbd
Jason <jasylwong@gmail.com>
2020-01-31 12:16:52 +0000

    Feature test added

---
+++ lib/feature_test.rb
@@ -0,0 +1,7 @@
+require './lib/oystercard.rb'
+require './lib/journey.rb'
+require './lib/station.rb'
+require './lib/journeylog.rb'
+
+
+
--- spec/feature_test_spec.rb
+++
@@ -1,8 +0,0 @@
-# require 'oystercard'
-
-# oystercard = Oystercard.new
-# oystercard.touch_in
-# # oystercard.in_journey?
-# # oystercard.touch_out
-
-
